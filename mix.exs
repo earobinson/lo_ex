09b91ad760419e2bc4689f6a6574a3b1fc387af2
Edward Robinson <earobinson@gmail.com>
2023-12-21 08:41:24 -0500

    Add LICENSE file and update mix.exs version

--- mix.exs
+++ mix.exs
@@ -4,10 +4,29 @@ defmodule LoEx.MixProject do
   def project do
     [
       app: :lo_ex,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp description do
+    """
+    Library for common Elixir functions.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Edward Robinson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/earobinson/loex"}
     ]
   end
 
@@ -22,7 +41,8 @@ defmodule LoEx.MixProject do
   defp deps do
     [
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:uniq, "~> 0.6.1"}
+      {:uniq, "~> 0.6.1"},
+      {:ex_doc, "~> 0.31.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
